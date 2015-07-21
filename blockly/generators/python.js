92cbaa76c8967a3a50dd256c067a6b4dbc62dcd8
Stephen Gilardi <sgilardi8@hotmail.com>
2015-07-21 15:46:58 -0700

    modified to only print imports when being used

--- blockly/generators/python.js
+++ blockly/generators/python.js
@@ -128,7 +128,7 @@ Blockly.Python.finish = function(code) {
       definitions.push(def);
     }
   }
-  var allDefs = 'import mcpi.minecraft as minecraft\nimport RPi.GPIO as GPIO\nimport mcpi.block as block\nGPIO.setmode(GPIO.BCM)\n' + imports.join('\n') + '\n\n' + definitions.join('\n\n') + '\nmc = minecraft.Minecraft.create()\n' + '\n\n';
+  var allDefs = 'import mcpi.minecraft as minecraft\nimport mcpi.block as block\n\n' + imports.join('\n') + '\n\n' + definitions.join('\n\n') + '\n\nmc = minecraft.Minecraft.create()\n' + '\n\n';
   return allDefs.replace(/\n\n+/g, '\n\n').replace(/\n*$/, '\n\n\n') +code;
 };
 
